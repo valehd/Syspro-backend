408208ed07f6172a4a741a14aacbf777cd09edf0
valehd <154106267+valehd@users.noreply.github.com>
2025-05-13 13:32:29 +0100

    Update tecnicos.js

--- routes/tecnicos.js
+++ routes/tecnicos.js
@@ -3,28 +3,19 @@ const router = express.Router()
 const tecnicoController = require('../controllers/tecnicoController')
 const db = require('../config/db')
 
-/**
- * GET /:id/etapas
- * Devuelve todas las etapas asignadas a un técnico específico
- */
-router.get('/:id/etapas', tecnicoController.obtenerEtapasPorTecnico)
+
 
 /**
  * GET /
  * Devuelve todos los técnicos (usuarios con rol "tecnico")
  */
-router.get('/', async (req, res) => {
-  try {
-    const [rows] = await db.query(`
-      SELECT id_usuario, nombre_usuario 
-      FROM Usuario 
-      WHERE rol = 'tecnico'
-    `)
-    res.json(rows)
-  } catch (err) {
-    console.error('Error al obtener técnicos:', err)
-    res.status(500).json({ error: 'Error al obtener técnicos' })
-  }
-})
+router.get('/', tecnicoController.obtenerTecnicos)
+
+
+/**
+ * GET /:id/etapas
+ * Devuelve todas las etapas asignadas a un técnico específico
+ */
+router.get('/:id/etapas', tecnicoController.obtenerEtapasPorTecnico)
 
 module.exports = router
